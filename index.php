b88020f680127509470ffcbc0844abe510fa2176
Timothy Monaghan <manyarms@gmail.com>
2013-11-27 23:07:37 -0800

    Still Trying to figure this out.

--- index.php
+++ index.php
@@ -1,2 +1,2 @@
-<?php $myvariable = "Is Git Working, Not Sure but I hope so, Please tell me!"?>
+<?php $myvariable = "Is Git Working, Not Sure but I hope so, Please tell me! So?"?>
 <?php echo $myvariable?>
